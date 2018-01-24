e20f2ecece60d878f28de7f233669229f5590487
quynhdn1 <QuynhDN1@fsoft.com.vn>
2018-01-24 23:15:23 +0700

    Modify testscript with simple search from gooogle

--- testscript1.rb
+++ testscript1.rb
@@ -1,31 +1,17 @@
-require "selenium-webdriver"
+require 'rubygems'
+require 'selenium-webdriver'
 
-#Firefox browser instantiation
 driver = Selenium::WebDriver.for :firefox
+driver.get "http://google.com"
 
-#Loading the assertselenium URL
-driver.navigate.to "http://www.assertselenium.com"
+element = driver.find_element :name => "q"
+element.send_keys "Cheese!"
+element.submit
 
-#Clicking on the Follow link present on the assertselenium home page
-FollowButton  = driver.find_element(:link, "Follow")
-FollowButton.click
+puts "Page title is #{driver.title}"
 
-#Typing the UserName
-LoginButton = driver.find_element(:id, "user_login")
-LoginButton.send_keys "sampleuser77dff27"
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "cheese!" }
 
-#Typing the Email-Id
-EmailId = driver.find_element(:id, "user_email")
-EmailId.send_keys "sampleuser7f7df27@gmail.com"
-
-#Clicking on the Submit Button
-SubmitButton = driver.find_element(:id, "wp-submit")
-SubmitButton.click
-
-#Asserting whether the registration success message is diaplyed
-SuccessMessage = driver.find_element(:css, "p.message")
-"Registration complete. Please check your e-mail.".eql? SuccessMessage.text
-puts "Successfully completed the user registration and validated the Success message"
-
-#Quitting the browser
+puts "Page title is #{driver.title}"
 driver.quit
